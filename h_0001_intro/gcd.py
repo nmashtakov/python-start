548d7b2946df74ce43ca6db806f5b2af99b84ca2
Nikita Mashtakov <nik_1612@mail.ru>
2023-02-21 23:16:19 +0300

    Удален exit и транслит в названиях переменных

--- h_0001_intro/gcd.py
+++ h_0001_intro/gcd.py
@@ -10,13 +10,12 @@ massStr = a.split(" ", 1)
 b = int(massStr[0])
 c = int(massStr[1])
 if max(b,c) % min(b,c) == 0:
-    print(c)
-    exit()
-ostatok = -1
+    NOD = c
+remainder = -1
 
-while ostatok != 0:
-    NOD = ostatok
-    ostatok = max(c,b) % min(c,b)
+while remainder != 0:
+    NOD = remainder
+    remainder = max(c,b) % min(c,b)
     b = min(c,b)
-    c = ostatok
+    c = remainder
 print(NOD)
